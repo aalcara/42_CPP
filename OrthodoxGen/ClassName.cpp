ca715d50cfd4d2c16392c1de6ea00a1bd1fd74d4
Anderson <ander_alvarenga@hotmail.com>
2022-03-09 15:57:24 -0300

    orthodox generator update

--- OrthodoxGen/ClassName.cpp
+++ OrthodoxGen/ClassName.cpp
@@ -2,26 +2,26 @@
 
 ClassName::ClassName()
 {
-	// std::cout << "Default constructor called" << std::endl;
+	// std::cout << "ClassName Default constructor called" << std::endl;
 	return ;
 }
 
 ClassName::~ClassName()
 {
-	// std::cout << "Destructor called" << std::endl;
+	// std::cout << "ClassName Destructor called" << std::endl;
 	return ;
 }
 
 ClassName::ClassName(const ClassName &Obj)
 {
-	// std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "ClassName Copy constructor called" << std::endl;
 	ClassName::operator=(Obj);
 	return ;
 }
 
 ClassName &ClassName::operator= (ClassName const &Obj)
 {
-	// std::cout << "Assigment operator called" << std::endl;
+	// std::cout << "ClassName Assigment operator called" << std::endl;
 	return (*this);
 }
 
