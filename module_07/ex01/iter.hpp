5002551a06b7535e8cc7de6e3fe080177b4894d1
Anderson <ander_alvarenga@hotmail.com>
2022-04-03 10:11:32 -0300

    m07ex01

---
+++ module_07/ex01/iter.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 09:57:41 by aalcara-          #+#    #+#             */
+/*   Updated: 2022/04/01 18:46:26 by aalcara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T>
+void iter(T *addr, size_t len, void (func)(T &member))
+{
+	for(size_t i = 0; i < len; i++)
+	{
+		func(addr[i]);
+	}
+}
+
+template <typename T>
+void increment(T &a)
+{
+	a += 1;
+}
+
+template <typename T>
+void append_a(T &a)
+{
+	a.append(1, 'a');
+}
+
+template <typename T>
+void duplicate(T &a)
+{
+	a.append(a);
+}
+
+template <typename T>
+void syscout(T &a)
+{
+	std::cout << a << std::endl;
+}
+
+#endif
---
+++ module_07/ex01/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 08:24:19 by aalcara-          #+#    #+#             */
+/*   Updated: 2022/04/01 18:46:53 by aalcara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	std::cout << "====== INT ======" << std::endl;
+	size_t len = 5;
+	int int_array[len];
+	for (size_t i = 0; i < len; i++)
+	{
+		int_array[i] = i * 10;
+	}
+
+	iter(int_array, len, syscout);
+	iter(int_array, len, increment);
+	std::cout << "--------------------" << std::endl;
+	iter(int_array, len, syscout);
+
+	std::cout << "====== CHAR ======" << std::endl;
+	char char_array[len];
+	for (size_t i = 0; i < len; i++)
+	{
+		char_array[i] = 'A' + i;
+	}
+	iter(char_array, len, syscout);
+	iter(char_array, len, increment);
+	std::cout << "--------------------" << std::endl;
+	iter(char_array, len, syscout);
+
+
+	std::cout << "====== STRING ======" << std::endl;
+	std::string string_arr[len];
+	for (size_t i = 0; i < len; i++)
+	{
+		string_arr[i].append(5, (i+'A'));
+	}
+
+	iter(string_arr, len, syscout);
+	std::cout << "--------------------" << std::endl;
+	iter(string_arr, len, append_a);
+	iter(string_arr, len, syscout);
+	std::cout << "--------------------" << std::endl;
+	string_arr[0].clear();
+	iter(string_arr, len, syscout);
+	std::cout << "--------------------" << std::endl;
+	iter(string_arr, len, append_a);
+	iter(string_arr, len, syscout);
+	std::cout << "--------------------" << std::endl;
+	iter(string_arr, len, duplicate);
+	iter(string_arr, len, syscout);
+
+	return (0);
+}
+
