f786243d1a62d27a207837ca16ccf63eb4a6c507
Anderson <ander_alvarenga@hotmail.com>
2022-04-03 10:12:14 -0300

    m07ex00

---
+++ module_07/ex00/whatever.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 09:57:41 by aalcara-          #+#    #+#             */
+/*   Updated: 2022/04/01 15:46:35 by aalcara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEMPLATE_HPP
+#define TEMPLADE_HPP
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+	if(a < b)
+		return (a);
+	return (b);
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+	if(a > b)
+		return (a);
+	return (b);
+}
+
+#endif
