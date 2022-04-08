e0632873373d3145ded650922aeff6a2621c2a27
Anderson <ander_alvarenga@hotmail.com>
2022-04-08 15:01:47 -0300

    m04 updates before evaluation

--- module_04/ex00/main.cpp
+++ module_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 11:22:57 by aalcara-          #+#    #+#             */
-/*   Updated: 2022/03/11 13:12:17 by aalcara-         ###   ########.fr       */
+/*   Updated: 2022/04/08 14:52:24 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int	main(void)
-{
-	Animal Zebra;
-	Dog Polvilho;
-	Cat Farinha;
-	std::cout << "-----------" << std::endl;
-	std::cout << Zebra.getType() << std::endl;
-	std::cout << Polvilho.getType() << std::endl;
-	std::cout << Farinha.getType() << std::endl;
-	std::cout << "-----------" << std::endl;
-	Zebra.makeSound();
-	Polvilho.makeSound();
-	Farinha.makeSound();
-	std::cout << "-----------" << std::endl;
+// int	main(void)
+// {
+// 	Animal zebra;
+// 	Dog dog;
+// 	Cat cat;
+// 	std::cout << "-----------" << std::endl;
+// 	std::cout << zebra.getType() << std::endl;
+// 	std::cout << dog.getType() << std::endl;
+// 	std::cout << cat.getType() << std::endl;
+// 	std::cout << "-----------" << std::endl;
+// 	zebra.makeSound();
+// 	dog.makeSound();
+// 	cat.makeSound();
+// 	std::cout << "-----------" << std::endl;
 
-	return (0);
-}
+// 	return (0);
+// }
 
 // int main()
 // {
@@ -49,20 +49,20 @@ int	main(void)
 // }
 
 
-// #include "WrongAnimal.hpp"
-// #include "WrongCat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
-// int main()
-// {
-// 	const WrongAnimal* meta = new WrongAnimal();
-// 	const WrongAnimal* i = new WrongCat();
+int main()
+{
+	const WrongAnimal* meta = new WrongAnimal();
+	const WrongAnimal* i = new WrongCat();
 
-// 	std::cout << i->getType() << " " << std::endl;
-// 	std::cout << meta->getType() << " " << std::endl;
-// 	i->makeSound(); //will output the cat sound!
-// 	meta->makeSound();
-// 	std::cout << "------" << std::endl;
-// 	delete meta;
-// 	delete i;
-// 	return 0;
-// }
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << meta->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	meta->makeSound();
+	std::cout << "------" << std::endl;
+	delete meta;
+	delete i;
+	return 0;
+}
